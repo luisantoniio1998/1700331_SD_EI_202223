fb2e17bafbb8e2913325cd3dddc199e4a3a2815b
Luís Barros <82943787+luisantoniio1998@users.noreply.github.com>
2023-04-11 12:44:38 +0100

    Aula 11 Abril

--- python/RPC/SimpleXMLRPCServer.py
+++ python/RPC/SimpleXMLRPCServer.py
@@ -19,9 +19,39 @@ with SimpleXMLRPCServer(('localhost', 8000),
         return x + y
     server.register_function(adder_function, 'add')
 
-    def chatGPT4(): 
-        return ("Inteligencia Artificial")
-    server.register_function(chatGPT4, 'ia')
+    def somatorio(n):
+        total = 0
+        for i in range(1,n+1):
+            total += i**3
+        return total
+    server.register_function(somatorio, 'sum')
+
+    def minus_function(x,y):
+        return x - y
+    server.register_function(minus_function, 'subtract')
+
+    def div_function(x,y):
+        return x // y
+    server.register_function(div_function, 'divide')
+    
+    def complex_num(x, y):
+        return str(complex(x, y)).replace('j', 'i')
+    
+    def sum_comple(a1,a2,b1,b2):
+        x = a1 + a2
+        y = b1 + b2 
+        return (complex_num(x,y))
+    server.register_function(sum_comple, 'soma_complexo')
+
+    def mul_comple(a1,a2,b1,b2):
+        x = (a1*a2)-(b1*b2)
+        y = (a2*b1)+(a1*b2)
+        return (complex_num(x,y))
+    server.register_function(mul_comple, 'multi_complexo')
+
+    #def chatGPT4(): 
+     #   return ("Inteligencia Artificial")
+    #server.register_function(chatGPT4, 'ia')
 
     # Register an instance; all the methods of the instance are
     # published as XML-RPC methods (in this case, just 'mul').
--- python/RPC/client.py
+++ python/RPC/client.py
@@ -1,13 +1,24 @@
 import xmlrpc.client
 
 s = xmlrpc.client.ServerProxy('http://localhost:8000')
-x= int(input("entre o 1-valor: "))
-y=int(input("entre o 2-valor: "))
+x= float(input("entre o 1-valor: "))
+y=float(input("entre o 2-valor: "))
+n= int(input("entre o valor n para fazer somatorio:"))
+a1 = float(input("parte real complexo 1: "))
+b1 = float(input("parte imaginaria complexo 1: "))
+a2 = float(input("parte real complexo 2: "))
+b2 = float(input("parte imaginaria complexo 2: "))
+       
 print(type(x))
-print(s.pow(x%150,y%10))  # Returns 2**3 = 8
-print(s.add(x,y))  # Returns 5
-print(s.mul(x,y))  # Returns 5*2 = 10
-print(s.ia()) #Returns inteligencia artificial
+print(s.pow(x%150,y%10))  # Returns x ^ y
+print(s.add(x,y))  # Returns x + y
+print(s.mul(x,y))  # Returns x * y
+print(s.subtract(x,y)) #Returns x - y 
+print(s.divide(x,y)) #Returns x / y 
+print(s.sum(n))
+print(s.soma_complexo(a1,a2,b1,b2))
+print(s.multi_complexo(a1,a2,b1,b2))
+#print(s.ia()) #Returns inteligencia artificial
 
 # Print list of available methods
 print(s.system.listMethods())
