40d897b187fbc8663265cc7471b8253aab4b23d1
Luís Barros <82943787+luisantoniio1998@users.noreply.github.com>
2023-05-08 00:15:15 +0100

    teste

---
+++ projeto/test.py
@@ -0,0 +1,11 @@
+//Criar um server RPC que fornece um menu para o cliente 
+
+*************************************************************
+1 - Obter informacao de um bloco de bitcoin e escrever num ficheiro txt
+2 - Serializar ficheiro criado 
+3 - Deserializar ficheiro serializado 
+4 - Sair 
+*************************************************************
+
+//No server rpc criar as funcoes correspondentes a cada opcao 
+
