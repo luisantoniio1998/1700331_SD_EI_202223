00c0b53318d0a94dcecadfa9b14e4727bfcb1308
Luís Barros <82943787+luisantoniio1998@users.noreply.github.com>
2023-05-08 21:06:14 +0100

    teste em serialization do ficheiro

--- projeto/RPCClient.py
+++ projeto/RPCClient.py
@@ -1,17 +1,30 @@
 import xmlrpc.client
+import sys
 
-def menu():
-    print("Serializar e Deserializar bloco Bitcoin")
-    print("********************************************")
+while True:
+    print("\n********************************************")
+    print("\nSERIALIZAR E DESERIALIZAR BLOCO DE BITCOIN")
+    print("\n********************************************")
     print("0. Receber informação de um bloco de bitcoin")
     print("1. Serializar ficheiro com informação do bloco")
     print("2. Deserializar ficheiro com informação do bloco")
-    print("*********************************************")
-    option = input("Enter an option: ")
+    print("3. Sair")
+    print("*********************************************\n")
+    option = input("Escolha uma opção: ")
     if option == "0":
-        hash_code = input("Enter the hash code: ")
+        hash_code = input("\nDigite o código hash: ")
         server = xmlrpc.client.ServerProxy("http://localhost:8000")
         response = server.get_block_info(hash_code)
         print(response)
+    elif option == "1":
+        hash_code = input("Digite o código hash pretendido :")
+        filename = filename = hash_code + ".txt"
+        server = xmlrpc.client.ServerProxy("http://localhost:8000")
+        response = server.serialize_file(filename)
+        print(response)
+    elif option == "3":
+        print("\nA sair do programa...")
+        break
+    else :
+        print("Opção errada, por favor escolha uma opção acima indicada") 
 
-menu()
--- projeto/RPCServer.py
+++ projeto/RPCServer.py
@@ -1,21 +1,38 @@
 import xmlrpc.server
 import requests
+import pickle
 
 class BlockInfoServer(xmlrpc.server.SimpleXMLRPCServer):
     def __init__(self, address):
         super().__init__(address, allow_none=True)
         self.register_function(self.get_block_info)
+        self.register_function(self.serialize_file)
+        self.register_function(self.deserialize_file)
 
     def get_block_info(self, hash_code):
         url = "https://blockchain.info/rawblock/{}".format(hash_code)
         response = requests.get(url)
-        if response.status_code == 200:
+        if response.status_code == 200: #200 -- resposta desejada do request
+
             data = response.json()
-            with open("{}.txt".format(hash_code), "w") as f:
+            filename = hash_code + ".txt"
+
+            #num_txns = data['n_tx']
+            with open(filename, "w") as f:
                 f.write(str(data))
-            return "Informação do bloco guardado em {}.txt".format(hash_code)
+                #f.write("Number of transactions: " + str(num_txns))
+            return "Informação do bloco guardado em " + filename
         else:
             return "Erro: Não foi possivel obter a informação do bloco desejado"
+    def serialize_file(self, filename):
+        with open(filename, "r") as f:
+            file_contents = f.read()
+        serialized_file = pickle.dumps(file_contents)
+        serialized_filename = filename + ".pickle"
+        with open(serialized_filename, "wb") as f:
+            f.write(serialized_file)
+        return "Ficheiro serializado guardado em : " + serialized_filename
+
 
 server = BlockInfoServer(("localhost", 8000))
 print("A receber na porta 8000...")
